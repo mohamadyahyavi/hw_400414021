45456f17d80beef4842d52a1ecd3816601d71a0c
mohammadyahyavi <mohamadyahyavi406@gmail.com>
2022-11-17 12:01:47 +0330

    3.c

---
+++ fekr2/3.c
@@ -0,0 +1,12 @@
+    c=c/10;
+            int k1=c%10;
+            int l1=c/10;
+            if(h==h1){
+        
+            e=e+1;
+            int m=h;
+                }
+            else if(h==l1 || h==i1 || h==j1 || h==k1 ){
+                d=d+1;
+            }
+            if(i==i1){
